100008705db5772cf2402bb79428629b313633bb
bkantiques <bkantiques@users.noreply.github.com>
2014-07-11 05:24:24 -0400

    Changed to PDO

--- addmooch.php
+++ addmooch.php
@@ -3,41 +3,47 @@ session_start();
 
 include 'databaseConnection.php';
 
-include 'funcs.php'; 
+if((!isset($_SESSION["un"])) || (!isset($_SESSION["userid"]))) {
+	header('Location: logout.php');
+	exit(); 
+	}
 
 if(isset($_POST['mooch_name'])) {
-	$mooch_name = sanitize($_POST['mooch_name']);
-	$userid = sanitize($_SESSION['userid']);
-	$friend_check_query = "SELECT COUNT(*) FROM Mooches WHERE UserID='$userid' AND Name='$mooch_name'";
-	$friend_check_result = mysql_query($friend_check_query);
-	$friend_check_row = mysql_fetch_array($friend_check_result);
-	//var_dump($friend_check_row);
-	if($friend_check_row['COUNT(*)'] === "1")
-		echo "You already have a mooch named " . $mooch_name;
-	else if($friend_check_row['COUNT(*)'] === "0") {
-		if($_POST['mooch_username'] != "" && $_POST['mooch_username'] != NULL) {
-			$mooch_username = sanitize($_POST['mooch_username']);
-			if($mooch_username == $_SESSION['un'])
+	$moochName = trim($_POST['mooch_name']);
+	$userid = $_SESSION['userid'];
+	//Check if you have any friends with this name already
+	$friendCheckQuery = $db->prepare("SELECT COUNT(*) FROM Mooches WHERE UserID=:userid AND Name=:moochName");
+	$friendCheckQuery->execute(array(":userid" => $userid, ":moochName" => $moochName));
+	$friendCheckResult = $friendCheckQuery->fetchAll(PDO::FETCH_ASSOC);
+	$friendCheckRow = $friendCheckResult[0];
+	if($friendCheckRow['COUNT(*)'] == 1)
+		echo "You already have a mooch named " . htmlspecialchars($moochName);
+	else if($friend_check_row['COUNT(*)'] == 0) {
+		if(trim($_POST['mooch_username']) != "" && $_POST['mooch_username'] != NULL) {
+			$moochUsername = trim($_POST['mooch_username']);
+			if($moochUsername == $_SESSION['un'])
 				echo "Can't have a mooch with your own username";
 			else {
-			$username_check_query = "SELECT Userid FROM Users WHERE Username='$mooch_username'";
-			$username_check_result = mysql_query($username_check_query);
-			$username_check_row = mysql_fetch_array($username_check_result);
+			$usernameCheckQuery = $db->prepare("SELECT Userid FROM Users WHERE Username=:mooch_username");
+			$usernameCheckQuery->execute(array(":mooch_username" => $moochUsername));
+			$usernameCheckResult = $usernameCheckQuery->fetchAll(PDO::FETCH_ASSOC);
+			$usernameCheckRow = $usernameCheckResult[0];
 				
-			if($username_check_row == FALSE)
+			if(!$usernameCheckRow)
 				echo "No users with that username";
 			else {
 		
-				$mooch_userid = $username_check_row['Userid'];
-				$userid_check_query = "SELECT COUNT(*) FROM Mooches WHERE MoochUserID='$mooch_userid' AND UserID='$userid'";
-				$userid_check_result = mysql_query($userid_check_query);
-				$userid_check_row = mysql_fetch_array($userid_check_result);
-				$userid_num = $userid_check_row['COUNT(*)'];
-				if($userid_num === "1")
+				$moochUserId = $usernameCheckRow['Userid'];
+				$useridCheckQuery = $db->prepare("SELECT COUNT(*) FROM Mooches WHERE MoochUserID=:mooch_userid AND UserID=:userid");
+				$useridCheckQuery->execute(array(":mooch_userid" => $moochUserId, "userid" => $userid));
+				$useridCheckResult = $useridCheckQuery->fetchAll(PDO::FETCH_ASSOC);
+				$useridCheckRow = $useridCheckResult[0];
+				$userid_num = $useridCheckRow['COUNT(*)'];
+				if($userid_num == 1)
 					echo "You already have a mooch with that username";
 				else {
-					$insert_query = "INSERT INTO Mooches (UserID, Name, MoochUserID) VALUES ('$userid', '$mooch_name', '$mooch_userid')";
-					mysql_query($insert_query);
+					$insertQuery = $db->prepare("INSERT INTO Mooches (UserID, Name, MoochUserID) VALUES (:userid, :mooch_name, :mooch_userid)");
+					$insertQuery->execute(array("userid" => $userid, ":mooch_name" => $moochName, ":mooch_userid" => $moochUserId));
 					header('Location: main.php');
 					exit();
 							}
@@ -50,8 +56,8 @@ if(isset($_POST['mooch_name'])) {
 			}
 			}
 			else {
-				$name_insert_query = "INSERT INTO Mooches (UserID, Name) VALUES ('$userid', '$mooch_name')";
-				mysql_query($name_insert_query);
+				$nameInsertQuery = $db->prepare("INSERT INTO Mooches (UserID, Name) VALUES (:userid, :mooch_name)");
+				$nameInsertQuery->execute(array("userid" => $userid, ":mooch_name" => $moochName));;
 				header('Location: main.php');
 				echo 'Problem';
 				}
