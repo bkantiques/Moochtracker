0ac7256c1696809615ee6da56b08de1ad02f303f
bkantiques <bkantiques@users.noreply.github.com>
2014-07-11 05:23:18 -0400

    Changed to PDO

--- moochInfo.php
+++ moochInfo.php
@@ -6,21 +6,24 @@ header('Location: index.php');
 exit();
 }
 
-
-
 include 'databaseConnection.php';
 
-include 'funcs.php';
-
-$userid = sanitize($_POST["UserID"]);
-$moochid = sanitize($_POST["MoochID"]);
+$userid = $_POST["UserID"];
+$moochid = $_POST["MoochID"];
 if(isset($_SESSION["MoochID"]))
 	unset($_SESSION["MoochID"]);
 $_SESSION["MoochID"] = $moochid;
-$moochName = sanitize($_POST["MoochName"]);
-$transactionQuery = "SELECT Amount, DateTime, Note FROM Transactions WHERE UserID=$userid AND MoochID=$moochid"; 
-$transactionResult = mysql_query($transactionQuery);
-$transactionRow = mysql_fetch_array($transactionResult);
+$moochNameQuery = $db->prepare("SELECT Name FROM Mooches WHERE MoochID=:moochid");
+$moochNameQuery->execute(array(":moochid" => $moochid));
+$moochNameResult = $moochNameQuery->fetchAll(PDO::FETCH_ASSOC);
+$moochNameRow = $moochNameResult[0];
+$moochName = $moochNameRow["Name"];
+
+//Get transactions between user and selected mooch
+$transactionQuery = $db->prepare("SELECT Amount, DateTime, Note FROM Transactions WHERE UserID=:userid AND MoochID=:moochid"); 
+$transactionQuery->execute(array(":userid" => $userid, ":moochid" => $moochid));
+$transactionResult = $transactionQuery->fetchAll(PDO::FETCH_ASSOC);
+$transactionRow = $transactionResult[0];
 if(!$transactionRow)
 	echo "<p>You have no transactions with " . $moochName . "</p>";
 else {
@@ -28,18 +31,20 @@ else {
 	$date;
 	$note;
 	$haveRows = true;
+	$i = 1;
 	echo "<h2>Transactions with " . $moochName . "</h2>";
 	echo "<table><tr><td class='amount'>Amount</td><td class='date'>Date</td><td class='note'>Note</td></tr> \n";
 	while($haveRows) {
 		$amount = $transactionRow["Amount"];
 		$date = $transactionRow["DateTime"];
-		$note = $transactionRow["Note"];
+		$note = htmlspecialchars($transactionRow["Note"]);
 		if($amount < 0)
 			echo "<tr><td class='amount'>Received \$" . abs($amount) . " </td>";
 		else
 			echo "<tr><td class='amount'>Gave \$$amount </td>";
 		echo "<td class='date'>$date</td><td class='note'>$note</td></tr>";
-		$transactionRow = mysql_fetch_array($transactionResult);
+		$transactionRow = $transactionResult[$i];
+		$i++;
 		if(!$transactionRow)
 			$haveRows = false;	
 	}
@@ -47,8 +52,8 @@ else {
 }
 echo "<h2>Add a transaction</h2>";
 echo "<form action='main.php' method='POST'>";
-echo "<label class='moochForm'>You </label> <select name='giveOrReceive'><option name=value='give'>give</option><option value='receive'>receive from</option></select> <label class='moochForm'>" . $moochName . "   \$</label> <input type='number' name='amount' min='.01' step='.01'>      <label class='moochForm' id='note'>Note: </label><textarea name='note' rows=4 cols=50 maxlength=250></textarea>  <input type='submit' id='addTransactionSubmit' value='Submit'>";
+echo "<label class='moochForm'>You </label> <select name='giveOrReceive'><option name=value='give'>give</option><option value='receive'>receive from</option></select> <label class='moochForm'>" . htmlspecialchars($moochName) . "   \$</label> <input type='number' name='amount' min='.01' step='.01'>      <label class='moochForm' id='note'>Note: </label><textarea name='note' rows=4 cols=50 maxlength=250></textarea>  <input type='submit' id='addTransactionSubmit' value='Submit'>";
 echo "</form>";
 
-mysql_close($link);
+$db = null;
 ?>
