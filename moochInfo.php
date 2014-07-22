43efbd3ef6b7effafed6ddba38f07c242168f417
bkantiques <bkantiques@users.noreply.github.com>
2014-07-22 05:29:46 -0400

    Added bootstrap styling, changed date and money display

--- moochInfo.php
+++ moochInfo.php
@@ -25,23 +25,28 @@ $transactionQuery->execute(array(":userid" => $userid, ":moochid" => $moochid));
 $transactionResult = $transactionQuery->fetchAll(PDO::FETCH_ASSOC);
 $transactionRow = $transactionResult[0];
 if(!$transactionRow)
-	echo "<p>You have no transactions with " . $moochName . "</p>";
+	echo "<h3>YOU HAVE NO TRANSACTION WITH " . $moochName . "</h3>";
 else {
 	$amount;
 	$date;
 	$note;
 	$haveRows = true;
 	$i = 1;
-	echo "<h2>Transactions with " . $moochName . "</h2>";
-	echo "<table><tr><td class='amount'>Amount</td><td class='date'>Date</td><td class='note'>Note</td></tr> \n";
+	echo "<h3>TRANSACTIONS WITH " . $moochName . "</h3>";
+	echo "<table class='table table-bordered'><thead><tr><th class='amount'>Amount</th><th class='date'>Date</th><th class='note'>Note</th></tr></thead> \n";
 	while($haveRows) {
 		$amount = $transactionRow["Amount"];
+		$amount= $amount/100;
 		$date = $transactionRow["DateTime"];
+		$timest = strtotime($date);
+		$date = date("D M j, Y    g:i A", $timest);
 		$note = htmlspecialchars($transactionRow["Note"]);
-		if($amount < 0)
-			echo "<tr><td class='amount'>Received \$" . abs($amount) . " </td>";
-		else
-			echo "<tr><td class='amount'>Gave \$$amount </td>";
+		if($amount < 0) {
+			$amount= number_format(abs($amount), 2);
+			echo "<tr><td class='amount'>Received \$" . $amount . " </td>";}
+		else {
+			$amount= number_format($amount, 2);
+			echo "<tr><td class='amount'>Gave \$$amount </td>";}
 		echo "<td class='date'>$date</td><td class='note'>$note</td></tr>";
 		$transactionRow = $transactionResult[$i];
 		$i++;
@@ -50,9 +55,9 @@ else {
 	}
 	echo "</table>";	
 }
-echo "<h2>Add a transaction</h2>";
-echo "<form action='main.php' method='POST'>";
-echo "<label class='moochForm'>You </label> <select name='giveOrReceive'><option name=value='give'>give</option><option value='receive'>receive from</option></select> <label class='moochForm'>" . htmlspecialchars($moochName) . "   \$</label> <input type='number' name='amount' min='.01' step='.01'>      <label class='moochForm' id='note'>Note: </label><textarea name='note' rows=4 cols=50 maxlength=250></textarea>  <input type='submit' id='addTransactionSubmit' value='Submit'>";
+echo "<h3>ADD A TRANSACTION</h3>";
+echo "<form class='form-inline' role='form' action='main.php' method='POST'>";
+echo "<div class='form-group'><select name='giveOrReceive' class='form-control'><option value='give'>You give</option><option value='receive'>You receive</option></select></div> <div class='form-group'><div class='input-group'><span class='input-group-addon'>\$</span><input type='number' class='form-control' name='amount' min='.01' step='.01'></div></div'>      <div class='form-group'><label class='moochForm' id='note'>Note: </label><textarea name='note' rows=1 class='form-control'></textarea></div><input type='submit' class='btn btn-primary' id='addTransactionSubmit' value='SUBMIT TRANSACTION'>";
 echo "</form>";
 
 $db = null;
